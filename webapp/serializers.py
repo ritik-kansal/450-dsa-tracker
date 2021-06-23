76c903dd8e602793230500722e6ad4c84f6d5afa
ritik-kansal <ritikkansal5@gmail.com>
2021-06-23 23:03:02 +0530

    get api for user data

--- webapp/serializers.py
+++ webapp/serializers.py
@@ -1,9 +1,15 @@
+from django.db.models import fields
+from webapp import models
 from rest_framework import serializers
-
-class UserSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=30)
-    user_name = serializers.CharField(max_length=30)
-    email = serializers.EmailField()
-    linkedin = serializers.CharField(max_length=255)
-    github = serializers.CharField(max_length=255)
-    password = serializers.CharField(max_length=255)
+from .models import User
+class UserSerializer(serializers.ModelSerializer):
+    # name = serializers.CharField(max_length=30)
+    # user_name = serializers.CharField(max_length=30)
+    # email = serializers.EmailField()
+    # linkedin = serializers.CharField(max_length=255)
+    # github = serializers.CharField(max_length=255)
+    # password = serializers.CharField(max_length=255)
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
--- webapp/views.py
+++ webapp/views.py
@@ -4,18 +4,20 @@ from rest_framework import serializers
 from .models import User
 from .serializers import UserSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
 def user_detail(request,id):
     user_data = User.objects.get(id=id)  
     serializer = UserSerializer(user_data)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type="application/json")
+    return JsonResponse(serializer.data)
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type="application/json")
 
 def all_user_detail(request):
     user_data = User.objects.all()  
     serializer = UserSerializer(user_data, many=True)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type="application/json")
+    return JsonResponse(serializer.data, safe=False)
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type="application/json")
