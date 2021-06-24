2a1566c004246b1a74a5cd2c53b6c48e1224daa4
ritik-kansal <ritikkansal5@gmail.com>
2021-06-24 23:03:21 +0530

    working in auth

--- dsa_450_tracker_backend/settings.py
+++ dsa_450_tracker_backend/settings.py
@@ -125,3 +125,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.BasicAuthentication',
+#         # 'rest_framework.authentication.SessionAuthentication',
+#     ],
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#         # 'rest_framework.authentication.SessionAuthentication',
+#     ]
+# }
+
+# AUTH_USER_MODEL = 'webapp.User'
--- webapp/models.py
+++ webapp/models.py
@@ -1,15 +1,60 @@
+from typing import AbstractSet
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models.base import ModelState
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
 # Create your models here.
-class User(models.Model):
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    username = None
     name = models.CharField(max_length=30)
-    user_name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=30,unique=True)
     email = models.EmailField()
     linkedin = models.CharField(max_length=255)
     github = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = ['email']
+
+    objects = CustomUserManager()
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=100)
