78ab754f56e5cdec5ea0389bd8b6d0b381113631
ritik <ritikkansal5@gmail.com>
2021-06-25 11:20:35 +0530

    secured secret key

--- dsa_450_tracker_backend/settings.py
+++ dsa_450_tracker_backend/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# BASE_DIR = os.path.dirname
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zydjbtv*mm9p8-03(89*3nn)))uyx3)0bjrui+h&jzoat2y*m2'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
