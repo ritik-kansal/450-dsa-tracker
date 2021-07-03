d3b5e730ce8857c9732a7c1964b313ed4e62a58d
KingK619 <bathinda880@gmail.com>
2021-07-03 16:55:52 +0530

    filters fixes 2

--- webapp/views/filter_views.py
+++ webapp/views/filter_views.py
@@ -1,7 +1,6 @@
 from .req import *
 
 class LevelFilterAPI(APIView):
-    serializer_class = QuestionSerializer
     permission_classes = [AllowAny]
     def get(self,request,id,Format=None):
         data = Question.filter(level=id)
@@ -9,24 +8,28 @@ class LevelFilterAPI(APIView):
         return Response(serializer.data)
 
 class StatusFilterAPI(APIView):
-    serializer_class = QuestionUserMarkSerializer
     def get(self,request,id,Format=None):
         data = Question_user_mark.objects.filter(mark=id)
         serializer = QuestionUserMarkSerializer(data,many=True)
         return Response(serializer.data)    
 
 class TopicFilterAPI(APIView):
-    serializer_class = TopicSerializer
+    permission_classes = [AllowAny]
     def get(self,request,id,Format=None):
         data = Question.objects.filter(topic_id=id)
         serializer=QuestionSerializer(data,many=True)
         return Response(serializer.data)
 
 class QuestionSearchFilterAPI(APIView):
-    serializer_class=QuestionSerializer
+    permission_classes = [AllowAny]
+    def get(self,request,Format=None):
+        data = Question.objects.all()
+        serializer=QuestionSerializer(data,many=True)
+        return Response(serializer.data)
+
     def post(self,request,Format=None):
         str=json.dumps(request.data)
         dict=json.loads(str)
-        data = Question.objects.filter(link=dict['link'])
+        data = Question.objects.filter(link__contains=dict['name'])
         serializer=QuestionSerializer(data)
         return Response(serializer.data)
