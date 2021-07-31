4e3cce2308f6c96d4421ce79cf57b1c7ae4d50ae
KingK619 <karanveersidana33@gmail.com>
2021-07-31 21:05:28 +0530

    nothing

--- webapp/views/profile_page_view.py
+++ webapp/views/profile_page_view.py
@@ -4,21 +4,36 @@ from .questions_solved_view import QuestionSolvedAPI
 from .filter_views import GeneralFilterAPI
 from django.db import connection
 
-import datetime,calendar
+import datetime
+import calendar
+
 
 class ProfilePageAPI(APIView):
-    def __dictfetchall(self,cursor):
-    # "Return all rows from a cursor as a dict"
+    def __dictfetchall(self, cursor):
+        # "Return all rows from a cursor as a dict"
         columns = [col[0] for col in cursor.description]
         return [
             dict(zip(columns, row))
             for row in cursor.fetchall()
         ]
 
+    def helper_mark(self, request, id=None):
+        if id == None:  # for current user
+            id = self.request.user.id
 
-    def get(self,request,id=None):
-        questions_solved = QuestionSolvedAPI().helper(request,1)
-        
+        mark = [0 for i in range(4)]
+        questions_marked = Question_user_mark.objects.all()
+        for question_mark in questions_marked:
+            print(question_mark.mark)
+            mark[question_mark.mark] += 1
+        mark[3] = 450-len(questions_marked)
+        return {
+            "marks": mark
+        }
+
+    def get(self, request, id=None):
+        questions_solved = QuestionSolvedAPI().helper(request, 1)
+        question_marked = self.helper_mark(request, id)
         today = datetime.date.today()
         idx = (today.weekday())
 
@@ -27,24 +42,25 @@ class ProfilePageAPI(APIView):
         sun = (today - datetime.timedelta(idx-6))
         sun_sql = sun.strftime('%Y-%m-%d')
 
-        query = (f"SELECT strftime('%Y-%m-%d',created_at) AS day, COUNT(*) as count FROM webapp_question_user_mark" 
-                f" WHERE created_at>='{mon_sql}' AND created_at<='{sun_sql}'"
-                  " GROUP BY day")
+        query = (f"SELECT strftime('%Y-%m-%d',created_at) AS day, COUNT(*) as count FROM webapp_question_user_mark"
+                 f" WHERE created_at>='{mon_sql}' AND created_at<='{sun_sql}'"
+                 " GROUP BY day")
 
         try:
             cursor = connection.cursor()
-            
+
             # cursor.execute(query,[mon,sun])
             cursor.execute(query)
             result = self.__dictfetchall(cursor)
             # print("before final")
             # print(result)
-            days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-            week_data = {days[i]:0 for i in range(7)}
+            days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            week_data = {days[i]: 0 for i in range(7)}
             # print(final_week_data)
 
             for r in result:
-                week_day = datetime.datetime.strptime(r['day'], '%Y-%m-%d').strftime('%a')
+                week_day = datetime.datetime.strptime(
+                    r['day'], '%Y-%m-%d').strftime('%a')
                 week_data[week_day] = r['count']
 
             for i in range(7):
@@ -54,12 +70,11 @@ class ProfilePageAPI(APIView):
             cursor.close()
             return Response({
                 "questions_solved": questions_solved,
-                "week_data":days
-            })
-            
-        except:
-            return Response({
-                "msg":"error occured"
+                "question_marked": question_marked,
+                "week_data": days
             })
 
-        
+        except:
+            return Response({
+                "msg": "error occured"
+            })
