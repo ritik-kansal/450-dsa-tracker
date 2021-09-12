c031db37f30aaf29f4b527920595ffe92b9e6bf7
ritik-kansal <ritikkansal5@gmail.com>
2021-09-13 00:16:04 +0530

    search bar

--- webapp/views/filter_views.py
+++ webapp/views/filter_views.py
@@ -1,3 +1,4 @@
+from django.http import response
 from .req import *
 from django.db import connection
 # filter question level based
@@ -55,6 +56,7 @@ class GeneralFilterAPI(APIView):
         ]
 
     def helper(self,request,page_number=1,format=None):
+        # print("api called")
         friends = Pair_programmer.objects.filter(user_1=request.user.id).select_related("user_2")
 
         sql_friends = ""
@@ -62,6 +64,7 @@ class GeneralFilterAPI(APIView):
             sql_friends += str(friend.user_2.id)+","
         sql_friends += str(friends[len(friends)-1].user_2.id)
 
+        # print("api called2")
          
         arguments = []
         select = (f"SELECT q.*,t.name as topic_name,qm.mark,(SELECT COUNT(*) FROM webapp_question_user_mark as qm WHERE qm.question_id = q.id AND qm.mark IN (0,1,2) AND user_id IN ({sql_friends})) AS friends")
@@ -71,10 +74,14 @@ class GeneralFilterAPI(APIView):
                   " JOIN webapp_topic as t ON q.topic_id = t.id"
                   " LEFT JOIN webapp_question_user_mark as qm ON q.id=qm.question_id")
         
+        # print("api called3")
         query_str = f" WHERE (user_id = {request.user.id} OR user_id IS NULL)"
         for query in request.data:
+            # print("api called4")
             if query!="search":
+                # print("api called5")
                 data = (request.data[query])
+                # print(data)
                 if "null" in data:
                     arguments+= [d for d in data if d!="null"]
                     query_str += f" AND ({query} IN ({','.join(['%s'] * (len(data)-1))}) OR {query} IS NULL)"
@@ -83,8 +90,9 @@ class GeneralFilterAPI(APIView):
                     query_str += f" AND {query} IN ({','.join(['%s'] * len(data))})"
             else:
                 for search in request.data[query]:
-                    arguments.append(request.data[query][search])
-                    query_str+=f" AND {search} LIKE '%%s%'"   
+                    # arguments.append(request.data[query][search])
+                    # print(request.data[query][search])
+                    query_str+=f" AND q.{search} LIKE '%%{request.data[query][search]}%%'"    
         try:
             cursor = connection.cursor()
             
@@ -96,20 +104,21 @@ class GeneralFilterAPI(APIView):
             arguments.append((page_number-1)*10)
             cursor.execute(select+table+query_str,arguments)
             result = self.__dictfetchall(cursor)
-
+            # print(select+table+query_str)
             cursor.close()
             return {
                 "length":len(result),
                 "total_length":result_count[0],
                 "questions":result
             }
+
         except:
-            return Response({
-                "msg":"error occured"
-            })
+            return {"msg":"error occured"}
 
     def post(self,request,page_number=1,format=None):
         # get friends for current user
-        return Response(self.helper(request,page_number,format))
+        response = self.helper(request,page_number,format)
+        # print(response)
+        return Response(response)
 
         # error handling
