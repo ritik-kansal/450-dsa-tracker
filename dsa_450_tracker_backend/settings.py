944991fd5e09f586667b1734624bd1e112f7de98
ritik-kansal <ritikkansal5@gmail.com>
2021-09-29 09:09:31 +0530

    heroku

--- dsa_450_tracker_backend/settings.py
+++ dsa_450_tracker_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://dsa-tracker-450.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
